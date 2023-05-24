8c88b21018a318deb2704842228e73e16bf437ac
meganano202 <meganano202@DESKTOP-222O0N1>
2023-05-24 20:00:50 +0200

    Todo arreglado

--- Tema 10.2/src/ejercicio8/Clima.java
+++ Tema 10.2/src/ejercicio8/Clima.java
@@ -1,10 +1,13 @@
 package ejercicio8;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Clima {
@@ -16,29 +19,35 @@ public class Clima {
 		Scanner sc = null;
 		try {
 			sc = new Scanner(new FileReader("Clima.csv"));
-			int filas = 0;
+			List<String> lineas = new ArrayList<>();
+			// Leer todas las lineas menos la primera y la última
 			while (sc.hasNextLine()) {
-				sc.nextLine();
-				filas++;
+				String linea = sc.nextLine();
+				lineas.add(linea);
+			}
+			if (lineas.size() > 1) {
+				lineas.remove(0); // Quita la primera linea
+				lineas.remove(lineas.size() - 1); // Quita la ultima linea
 			}
 
-			String[][] nuevo = new String[filas-1][];
-			
-			sc = new Scanner(new FileReader("Clima.csv"));
-			sc.nextLine();
-			
-			for (int i = 0; sc.hasNextLine(); i++) {
-				String linea = sc.nextLine();
-				String[] temp = linea.split(",");
-				nuevo[i] = temp;
+			int numFilas = lineas.size();
+			int numColumnas = 3; // Siempre hay 3 columnas
+			String[][] nuevo = new String[numFilas][numColumnas];
+
+			// Pasa la colección a un array
+			for (int rowIndex = 0; rowIndex < numFilas; rowIndex++) {
+				String line = lineas.get(rowIndex);
+				String[] lineArray = line.split(",");
+				nuevo[rowIndex] = lineArray;
 			}
 
 			this.tiempo = nuevo;
-
 		} catch (FileNotFoundException e) {
 			System.out.println("ERROR: Archivo no encontrado");
 		} finally {
-			sc.close();
+			if (sc != null) {
+				sc.close();
+			}
 		}
 	}
 
@@ -53,23 +62,22 @@ public class Clima {
 
 	public void añadir(String fecha, int max, int min) {
 		boolean existe = false;
-		for (int i = 0; i < tiempo.length && existe == false; i++) {
+		for (int i = 0; i < tiempo.length && !existe; i++) {
 			if (tiempo[i][0].equals(fecha)) {
 				existe = true;
 			}
 		}
-		if (existe == false) {
+		if (!existe) {
 			int filas = tiempo.length;
-			int col = tiempo.length > 0 ? tiempo[0].length : 3;
 			String[][] nuevo = new String[filas + 1][3];
 
-			for (int i = 0; i < filas; i++) {
-				System.arraycopy(tiempo[i], 0, nuevo[i], 0, col);
+			for (int i = 0; i < filas - 1; i++) {
+				System.arraycopy(tiempo[i], 0, nuevo[i], 0, 3);
 			}
 
 			String[] lineaAñadir = { fecha, String.valueOf(max), String.valueOf(min) };
 
-			nuevo[filas] = lineaAñadir;
+			nuevo[filas - 1] = lineaAñadir;
 
 			tiempo = nuevo;
 		} else {
@@ -80,19 +88,56 @@ public class Clima {
 	public void mostrar() {
 		System.out.println("Fecha,Temperatura máxima,Temperatura mínima");
 		for (int i = 0; i < tiempo.length; i++) {
-			for (int j = 0; j < tiempo[0].length; j++) {
-				System.out.print(j == 1 || j == 0 ? tiempo[i][j] + "," : tiempo[i][j]);
-				if (i >= 1 && j == 1 && this.max < Integer.valueOf(tiempo[i][j])) {
-					this.max = Integer.valueOf(tiempo[i][j]);
+			if (tiempo[i][0] != null) {
+				for (int j = 0; j < tiempo[i].length; j++) {
+					System.out.print(tiempo[i][j]);
+					if (j < tiempo[i].length - 1) {
+						System.out.print(",");
+					}
+					if (j == 1 && this.max < Integer.parseInt(tiempo[i][j])) {
+						this.max = Integer.parseInt(tiempo[i][j]);
+					}
+					if (j == 2 && this.min > Integer.parseInt(tiempo[i][j])) {
+						this.min = Integer.parseInt(tiempo[i][j]);
+					}
 				}
-				if (i >= 1 && j == 2 && this.min > Integer.valueOf(tiempo[i][j])) {
-					this.min = Integer.valueOf(tiempo[i][j]);
+				System.out.println();
+			}
+		}
+		System.out.println();
+		System.out.println("La temperatura máxima ha sido: " + max);
+		System.out.println("La temperatura mínima ha sido: " + min);
+	}
+
+	public void checkTemp() {
+		BufferedReader fw = null;
+
+		try {
+			fw = new BufferedReader(new FileReader("Clima.csv"));
+			String linea;
+			fw.readLine();
+
+			while ((linea = fw.readLine()) != null) {
+				String[] partes = linea.split(",");
+				int max = Integer.parseInt(partes[1]);
+				int min = Integer.parseInt(partes[2]);
+
+				if (max > this.max) {
+					this.max = max;
+				}
+				if (min < this.min) {
+					this.min = min;
 				}
 			}
-			System.out.println();
+		} catch (IOException e) {
+			System.out.println("ERROR: No se ha podido encontrar el archivo");
+		} finally {
+			try {
+				fw.close();
+			} catch (IOException e) {
+				System.out.println("ERROR: No se ha podido cerrar el archivo");
+			}
 		}
-		System.out.println("La temperatura maxima ha sido: " + max);
-		System.out.println("La temperatura minima ha sido: " + min);
 	}
 
 	public void salir() {
@@ -102,28 +147,31 @@ public class Clima {
 			fw.write("Fecha,Temperatura máxima,Temperatura mínima");
 			fw.newLine();
 			for (int i = 0; i < tiempo.length; i++) {
-				for (int j = 0; j < tiempo[0].length; j++) {
-					if (j == 0 || j == 1) {
-						fw.write(tiempo[i][j] + ",");
-					} else {
+				if (tiempo[i][0] != null) {
+					for (int j = 0; j < tiempo[i].length; j++) {
 						fw.write(tiempo[i][j]);
+						if (j < tiempo[i].length - 1) {
+							fw.write(",");
+						}
 					}
+					fw.newLine();
 				}
-				fw.newLine();
 			}
+
+			fw.newLine();
+			fw.write("Temp. Max: " + String.valueOf(max) + " Temp. Min: " + String.valueOf(min));
 		} catch (IOException e) {
 			System.out.println("ERROR: Archivo no encontrado");
 		} finally {
 			try {
-				fw.flush();
-			} catch (IOException e) {
-				System.out.println("ERROR: No se ha podido realizar el flush");
-			}
-			try {
-				fw.close();
+				if (fw != null) {
+					fw.flush();
+					fw.close();
+				}
 			} catch (IOException e) {
 				System.out.println("ERROR: No se ha podido cerrar el archivo");
 			}
 		}
 	}
+
 }
